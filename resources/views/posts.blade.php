38cf86de9b9d442ff61cdaf0bf5a89dec3270263
vannesa <manelp3214@gmail.com>
2021-02-20 18:42:29 -0600

    finalizo otro video

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,14 +5,19 @@
      <div class="row justify-content-center">
          <div class="col-md-8">
              @foreach($posts as $post)
-             <div class="card">
+             <div class="card mb-4">
                   <div class="card-body">
                   <h5 class="card-title">{{ $post->title }}</h5>
                      <p class="card-text">
                          {{ $post->get_excerpt }}
                          <a href="{{ route('post', $post) }}">Leer mas</a>
-
                      </p>
+                         <p class="text-muted mb-0">
+                             <em>
+                             &ndash; {{ $post->user->name }}
+                            </em>
+                               {{ $post->created_at->format('d M y') }}
+                         </p>
                  </div>
              </div>
              @endforeach
