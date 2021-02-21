e6b5488855cf1a973dc14f45744b9026c992e552
vannesa <manelp3214@gmail.com>
2021-02-20 20:33:21 -0600

    terminamos vista y diseño

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -16,8 +16,39 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    You are logged in!
+                         <table class="table">
+                             <thead>
+                                 <tr>
+                                     <th>ID</th>
+                                     <th>Titulo</th>
+                                     <th colspan="2">&nbsp;</th>
+                                 </tr>
+                             </thead>
+                            <tbody>
+                            @foreach ($posts as $post)
+                            <tr>
+                                 <td>{{ $post->id }}</td>
+                                 <td>{{ $post->title }}</td>
+                                 <td>
+                                      <a href="{{ route('posts.edit', $post) }}" clas="btn btn-primary btn-sm">
+                                         Editar
+                                      </a>
+                                 </td>
+                                <td>
+                                   <form action="{{ route('posts.destroy', $post) }}" method="POST">
+                                       @csrf
+                                       @method('DELETE')
+                                       <input
+                                       type="submit"
+                                       value="Eliminar"
+                                       class="btn btn-sm btn-danger"
+                                       onclick="return confirm('¿Desea eliminar..?')">
+                                   </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                            </tbody>
+                         </table>
                 </div>
             </div>
         </div>
