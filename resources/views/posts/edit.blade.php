a13e7b02ff95f279403aabacb5aa8f65e539532d
vannesa <manelp3214@gmail.com>
2021-02-22 20:14:22 -0600

    continuamos

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -31,7 +31,7 @@
 
                              <div class="form-group">
                                   <label>Contenido embebido</label>
-                                  <textarea name="iframe" class="form-control" required>{{ old('iframe', $post->iframe) }}</textarea>
+                                  <textarea name="iframe" class="form-control" >{{ old('iframe', $post->iframe) }}</textarea>
                              </div>
 
                              <div class="form-group">
